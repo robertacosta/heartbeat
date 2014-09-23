b27f57837709f1f14f6be52f50e973a848ae573f
Robert Acosta <robert.j.acosta@gmail.com>
2014-09-22 22:35:07 -0700

    Corrects ID annotation

--- src/main/java/emr/acl/Principle.java
+++ src/main/java/emr/acl/Principle.java
@@ -8,10 +8,9 @@ import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
-
-import org.springframework.data.annotation.Id;
 
 @Entity
 @Table(
